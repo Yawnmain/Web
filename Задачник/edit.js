15d26a935a8132b16b0f971c64a31750c0df34ed
Yawnmain <108503319+Yawnmain@users.noreply.github.com>
2022-12-12 19:18:59 +0800

    Done
    Done

---
+++ Задачник/edit.js
@@ -0,0 +1,186 @@
+function red() {
+    document.getElementById('win').style.display='block';
+};
+
+function rem() {
+    document.getElementById('1').remove();
+};
+
+function addText() {
+    document.getElementById('p1').innerHTML = document.getElementById('task_text').value;
+    if (document.getElementById('high').checked){
+        document.getElementById('hi_img').style.display='inline-flex';
+    }
+    document.getElementById('win').style.display='none';
+    document.getElementById('task_text').value = "";
+};
+
+function red2() {
+    document.getElementById('win2').style.display='block';
+};
+
+function rem2() {
+    document.getElementById('2').remove();
+};
+
+function addText2() {
+    document.getElementById('p2').innerHTML = document.getElementById('task_text2').value;
+    if (document.getElementById('high2').checked){
+        document.getElementById('hi_img2').style.display='inline-flex';
+    }
+    document.getElementById('win2').style.display='none';
+    document.getElementById('task_text2').value = "";
+};
+
+function red3() {
+    document.getElementById('win3').style.display='block';
+};
+
+function rem3() {
+    document.getElementById('3').remove();
+};
+
+function addText3() {
+    document.getElementById('p3').innerHTML = document.getElementById('task_text3').value;
+    if (document.getElementById('high3').checked){
+        document.getElementById('hi_img3').style.display='inline-flex';
+    }
+    document.getElementById('win3').style.display='none';
+    document.getElementById('task_text3').value = "";
+};
+
+function red4() {
+    document.getElementById('win4').style.display='block';
+};
+
+function rem4() {
+    document.getElementById('4').remove();
+};
+
+function addText4() {
+    document.getElementById('p4').innerHTML = document.getElementById('task_text4').value;
+    if (document.getElementById('high4').checked){
+        document.getElementById('hi_img4').style.display='inline-flex';
+    }
+    document.getElementById('win4').style.display='none';
+    document.getElementById('task_text4').value = "";
+};
+
+function red5() {
+    document.getElementById('win5').style.display='block';
+};
+
+function rem5() {
+    document.getElementById('5').remove();
+};
+
+function addText5() {
+    document.getElementById('p5').innerHTML = document.getElementById('task_text5').value;
+    if (document.getElementById('high5').checked){
+        document.getElementById('hi_img5').style.display='inline-flex';
+    }
+    document.getElementById('win5').style.display='none';
+    document.getElementById('task_text5').value = "";
+};
+
+function red6() {
+    document.getElementById('win6').style.display='block';
+};
+
+function rem6() {
+    document.getElementById('6').remove();
+};
+
+function addText6() {
+    document.getElementById('p6').innerHTML = document.getElementById('task_text6').value;
+    if (document.getElementById('high6').checked){
+        document.getElementById('hi_img6').style.display='inline-flex';
+    }
+    document.getElementById('win6').style.display='none';
+    document.getElementById('task_text6').value = "";
+};
+
+function red7() {
+    document.getElementById('win7').style.display='block';
+};
+
+function rem7() {
+    document.getElementById('7').remove();
+};
+
+function addText7() {
+    document.getElementById('p7').innerHTML = document.getElementById('task_text7').value;
+    if (document.getElementById('high7').checked){
+        document.getElementById('hi_img7').style.display='inline-flex';
+    }
+    document.getElementById('win7').style.display='none';
+    document.getElementById('task_text7').value = "";
+};
+
+function red8() {
+    document.getElementById('win8').style.display='block';
+};
+
+function rem8() {
+    document.getElementById('8').remove();
+};
+
+function addText8() {
+    document.getElementById('p8').innerHTML = document.getElementById('task_text8').value;
+    if (document.getElementById('high8').checked){
+        document.getElementById('hi_img8').style.display='inline-flex';
+    }
+    document.getElementById('win8').style.display='none';
+    document.getElementById('task_text8').value = "";
+};
+
+function red9() {
+    document.getElementById('win9').style.display='block';
+};
+
+function rem9() {
+    document.getElementById('9').remove();
+};
+
+function addText9() {
+    document.getElementById('p9').innerHTML = document.getElementById('task_text9').value;
+    if (document.getElementById('high9').checked){
+        document.getElementById('hi_img9').style.display='inline-flex';
+    }
+    document.getElementById('win9').style.display='none';
+    document.getElementById('task_text9').value = "";
+};
+
+function red10() {
+    document.getElementById('win10').style.display='block';
+};
+
+function rem10() {
+    document.getElementById('10').remove();
+};
+
+function addText10() {
+    document.getElementById('p10').innerHTML = document.getElementById('task_text10').value;
+    if (document.getElementById('high10').checked){
+        document.getElementById('hi_img10').style.display='inline-flex';
+    }
+    document.getElementById('win10').style.display='none';
+    document.getElementById('task_text10').value = "";
+};
+
+function red11() {
+    document.getElementById('win11').style.display='block';
+};
+
+function rem11() {
+    document.getElementById('11').remove();
+};
+
+function addText11() {
+    document.getElementById('p11').innerHTML = document.getElementById('task_text11').value;
+    if (document.getElementById('high11').checked){
+        document.getElementById('hi_img11').style.display='inline-flex';
+    }
+    document.getElementById('win11').style.display='none';
+    document.getElementById('task_text11').value = "";
+};
---
+++ Задачник/script.js
@@ -0,0 +1,43 @@
+var index = 1;
+function win1() {
+    var limit = 2;
+    for (var i = 1; i < limit;i++){
+        document.getElementById(index).style.display="inline-flex";
+    }
+    index += 1;
+};
+
+
+function cancel() {
+    document.getElementById("win").style.display='none';
+}
+function cancel2() {
+    document.getElementById("win2").style.display='none';
+}
+function cancel3() {
+    document.getElementById("win3").style.display='none';
+}
+function cancel4() {
+    document.getElementById("win4").style.display='none';
+}
+function cancel5() {
+    document.getElementById("win5").style.display='none';
+}
+function cancel6() {
+    document.getElementById("win6").style.display='none';
+}
+function cancel7() {
+    document.getElementById("win7").style.display='none';
+}
+function cancel8() {
+    document.getElementById("win8").style.display='none';
+}
+function cancel9() {
+    document.getElementById("win9").style.display='none';
+}
+function cancel10() {
+    document.getElementById("win10").style.display='none';
+}
+function cancel11() {
+    document.getElementById("win11").style.display='none';
+}
